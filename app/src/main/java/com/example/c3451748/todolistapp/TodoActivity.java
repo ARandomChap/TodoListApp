e40b941d1139979fc63d831be20dce4eeed972cf
Jack Chapman <jackchapman1997@hotmail.co.uk>
2017-11-22 20:09:16 +0000

    attempt at adding toolbar

--- app/src/main/java/com/example/c3451748/todolistapp/TodoActivity.java
+++ app/src/main/java/com/example/c3451748/todolistapp/TodoActivity.java
@@ -5,15 +5,31 @@ package com.example.c3451748.todolistapp;
  */
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
+
 public class TodoActivity extends AppCompatActivity {
+
+    DbHelper dbHelper;
+    ArrayAdapter<String> mAdapter;
+    ListView lstTask;
+
 
     public static final String EXTRA_TODO_ID = "todo_id";
 
@@ -36,9 +52,15 @@ public class TodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.action_add_task);
+//        setSupportActionBar(myToolbar);
+
+        dbHelper = new DbHelper(this);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        loadTaskList();
 
         if (fragment == null){
 
@@ -49,6 +71,56 @@ public class TodoActivity extends AppCompatActivity {
                     .commit();
         }
 
+    }
+
+    private void loadTaskList() {
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if(mAdapter==null){
+            mAdapter = new ArrayAdapter<String>(this,R.layout.row,R.id.task_title,taskList);
+            lstTask.setAdapter(mAdapter);
+        }
+        else{
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar,menu);
+
+        //Change menu icon color
+        Drawable icon = menu.getItem(0).getIcon();
+        icon.mutate();
+        icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add New Task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dbHelper.insertNewTask(task);
+                                loadTaskList();
+                            }
+                        })
+                        .setNegativeButton("Cancel",null)
+                        .create();
+                dialog.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
